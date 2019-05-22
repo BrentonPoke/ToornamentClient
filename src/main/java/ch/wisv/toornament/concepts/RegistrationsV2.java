bd6477157c64f94ead5c9df2bc1c49ba30349a33
Brenton <brentonpoke@outlook.com>
2019-05-22 09:29:46 -0400

    Post method for registrations.

--- src/main/java/ch/wisv/toornament/concepts/RegistrationsV2.java
+++ src/main/java/ch/wisv/toornament/concepts/RegistrationsV2.java
@@ -1,20 +1,55 @@
 package ch.wisv.toornament.concepts;
 
 import ch.wisv.toornament.ToornamentClient;
+import ch.wisv.toornament.exception.ToornamentException;
+import ch.wisv.toornament.model.Participant;
+import ch.wisv.toornament.model.TeamParticipant;
 import ch.wisv.toornament.model.enums.Scope;
+import java.io.IOException;
+import java.util.List;
 import java.util.Map;
 import okhttp3.HttpUrl;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class RegistrationsV2 extends Concept {
-    private String tournamentID;
-    public RegistrationsV2(ToornamentClient client, String tournamentID) {
-        super(client);
-        this.tournamentID = tournamentID;
-    }
+  private String tournamentID;
 
-    private Request getRegistrations(Map<String, String> header, Map<String, String> paramsMap) {
-        HttpUrl.Builder url = new HttpUrl.Builder();
+  public RegistrationsV2(ToornamentClient client, String tournamentID) {
+    super(client);
+    this.tournamentID = tournamentID;
+  }
+
+  public TeamParticipant register(Participant participant) {
+    HttpUrl.Builder url = new HttpUrl.Builder();
+    if (client.getScope().contains(Scope.ORGANIZER_REGISTRATION)) {
+      url.scheme("https")
+          .host("api.toornament.com")
+          .addEncodedPathSegment("organizer")
+          .addEncodedPathSegment("v2")
+          .addEncodedPathSegment("tournaments")
+          .addEncodedPathSegment(tournamentID)
+          .addEncodedPathSegment("registrations");
+    }
+    RequestBody body =
+        RequestBody.create(MediaType.parse("application/json"), participant.toString());
+    Request request = client.getRequestBuilder()
+        .post(body)
+        .url(url.build())
+        .build();
+    try {
+      String responseBody = client.executeRequest(request).body().string();
+      return mapper.readValue(responseBody, mapper.constructType(TeamParticipant.class));
+    } catch (IOException | NullPointerException e) {
+      System.out.println(e.getMessage());
+      throw new ToornamentException("Got Excption posting Participant");
+    }
+  }
+
+  public List<TeamParticipant> getRegistrations(
+      Map<String, String> header, Map<String, String> paramsMap) {
+    HttpUrl.Builder url = new HttpUrl.Builder();
     if (client.getScope().contains(Scope.ORGANIZER_REGISTRATION)) {
       url.scheme("https")
           .host("api.toornament.com")
@@ -26,12 +61,26 @@ public class RegistrationsV2 extends Concept {
       for (Map.Entry<String, String> params : paramsMap.entrySet()) {
         url.addQueryParameter(params.getKey(), params.getValue());
       }
-        }
-        return client.getRequestBuilder()
+    }
+    Request request =
+        client
+            .getRequestBuilder()
             .get()
             .url(url.build())
             .addHeader("range", header.get("range"))
             .build();
-    }
+    return getTeamParticipantsHelper(request);
+  }
 
+  private List<TeamParticipant> getTeamParticipantsHelper(Request request) {
+    try {
+      String responseBody = client.executeRequest(request).body().string();
+      return mapper.readValue(
+          responseBody,
+          mapper.getTypeFactory().constructCollectionType(List.class, TeamParticipant.class));
+    } catch (IOException | NullPointerException e) {
+      System.out.println(e.getMessage());
+      throw new ToornamentException("Got Excption getting Participants");
+    }
+  }
 }
