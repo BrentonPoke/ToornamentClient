510541b74c9f7d2ed2944b36836178f3939c762b
Brenton Poke <brentonpoke@outlook.com>
2019-11-30 21:01:28 -0500

    classes and enum

--- src/main/java/ch/wisv/toornament/concepts/PermissionsV2.java
+++ src/main/java/ch/wisv/toornament/concepts/PermissionsV2.java
@@ -1,5 +1,45 @@
 package ch.wisv.toornament.concepts;
 
-public class PermissionsV2 {
+import ch.wisv.toornament.ToornamentClient;
+import ch.wisv.toornament.exception.ToornamentException;
+import ch.wisv.toornament.model.Permissions;
+import ch.wisv.toornament.model.enums.Scope;
+import java.io.IOException;
+import java.util.List;
+import okhttp3.HttpUrl.Builder;
+import okhttp3.Request;
 
+public class PermissionsV2 extends Concept {
+    private String tournamentID;
+    public PermissionsV2(ToornamentClient client, String tournamentID) {
+        super(client);
+        this.tournamentID = tournamentID;
+    }
+
+
+    List<Permissions> getPermissions(){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_PERMISSION)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("permissions");
+        }
+        Request request = client.getRequestBuilder()
+            .get()
+            .url(url.build())
+            .build();
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructCollectionType(List.class, Permissions.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOExcption getting Permissions");
+        }
+    }
 }
--- src/main/java/ch/wisv/toornament/model/Permissions.java
+++ src/main/java/ch/wisv/toornament/model/Permissions.java
@@ -1,5 +1,15 @@
 package ch.wisv.toornament.model;
 
+import ch.wisv.toornament.model.enums.Attribute;
+import java.util.List;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class Permissions {
+    private List<Attribute> attributes;
+    private String email;
+    private String id;
 
 }
