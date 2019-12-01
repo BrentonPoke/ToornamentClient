1e7c96bd1e5ff3c4ce6a6fa85de028e6227c8b0e
Brenton Poke <brentonpoke@outlook.com>
2019-12-01 02:03:49 -0500

    updatePermission()

--- src/main/java/ch/wisv/toornament/concepts/PermissionsV2.java
+++ src/main/java/ch/wisv/toornament/concepts/PermissionsV2.java
@@ -3,9 +3,12 @@ package ch.wisv.toornament.concepts;
 import ch.wisv.toornament.ToornamentClient;
 import ch.wisv.toornament.exception.ToornamentException;
 import ch.wisv.toornament.model.Permissions;
-import ch.wisv.toornament.model.TeamParticipant;
+import ch.wisv.toornament.model.enums.Attribute;
 import ch.wisv.toornament.model.enums.Scope;
 import ch.wisv.toornament.model.request.PermissionsQuery;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.IOException;
 import java.util.List;
 import okhttp3.HttpUrl.Builder;
@@ -98,6 +101,44 @@ public class PermissionsV2 extends Concept {
         } catch (IOException | NullPointerException e) {
             System.out.println(e.getMessage());
             throw new ToornamentException("Error creating new permission");
+        }
+    }
+
+    Permissions updatePermissions(List<Attribute> attributes, String permissionID){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_PERMISSION)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("permissions")
+                .addEncodedPathSegment(permissionID);
+        }
+
+        String attributeString = "";
+        try{
+            attributeString= new ObjectMapper().setSerializationInclusion(
+                JsonInclude.Include.NON_NULL).writeValueAsString(attributes);
+        }catch (JsonProcessingException e){
+            throw new ToornamentException("Couldn't create string from List of Attributes: " + e.getMessage());
+        }
+
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),attributeString);
+        Request request = client.getRequestBuilder()
+            .patch(body)
+            .url(url.build())
+            .build();
+
+        try{
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructType(Permissions.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Error updating new permission");
         }
     }
 }
