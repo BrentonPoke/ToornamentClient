f6b771ea3b840bb1db6a8b040cb864e98545fb8f
Brenton <brentonpoke@outlook.com>
2019-12-06 19:18:08 -0500

    #12 implementation

--- src/main/java/com/toornament/concepts/StreamsV2.java
+++ src/main/java/com/toornament/concepts/StreamsV2.java
@@ -2,14 +2,18 @@ package com.toornament.concepts;
 
 import com.toornament.ToornamentClient;
 import com.toornament.exception.ToornamentException;
+import com.toornament.model.Custom.Custom;
 import com.toornament.model.Stream;
 import com.toornament.model.Tournament;
+import com.toornament.model.Video;
 import com.toornament.model.enums.Scope;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
 import okhttp3.HttpUrl.Builder;
+import okhttp3.MediaType;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 
 public class StreamsV2 extends Concept{
     private String tournamentID;
@@ -42,6 +46,170 @@ public class StreamsV2 extends Concept{
         } catch (IOException e) {
             e.getMessage();
             throw new ToornamentException("Couldn't retrieve streams");
+        }
+    }
+
+    public Stream createStream(Video query){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("streams");
+        }
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),query.toString());
+        Request request = client.getAuthenticatedRequestBuilder()
+        .post(body)
+        .url(url.build())
+        .build();
+
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructType(Stream.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOException creating stream");
+        }
+    }
+
+    public Stream getStream(String streamID){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("streams")
+                .addEncodedPathSegment(streamID);
+        }
+
+        Request request = client.getAuthenticatedRequestBuilder()
+            .get()
+            .url(url.build())
+            .build();
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructType(Stream.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOException getting stream");
+        }
+    }
+
+    public Stream updateStream(String streamID, Video query){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("streams")
+                .addEncodedPathSegment(streamID);
+        }
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),query.toString());
+        Request request = client.getAuthenticatedRequestBuilder()
+            .patch(body)
+            .url(url.build())
+            .build();
+
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructType(Stream.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOException creating stream");
+        }
+    }
+
+    public Integer deleteStream(String streamID){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment("streams")
+                .addEncodedPathSegment(streamID);
+        }
+        Request request = client.getAuthenticatedRequestBuilder()
+            .delete()
+            .url(url.build())
+            .build();
+
+        return client.executeRequest(request).code();
+    }
+
+    public List<String> getStreamsOfMatch(String matchID){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment(matchID)
+                .addEncodedPathSegment("streams");
+        }
+
+        Request request = client.getAuthenticatedRequestBuilder()
+            .get()
+            .url(url.build())
+            .build();
+
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructCollectionType(List.class,String.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOException getting match streams");
+        }
+    }
+
+    public List<Stream> updateMatchStreams(String matchID, List<String> streamList){
+        Builder url = new Builder();
+        if(client.getScope().contains(Scope.ORGANIZER_ADMIN)){
+            url
+                .scheme("https")
+                .host("api.toornament.com")
+                .addEncodedPathSegment("organizer")
+                .addEncodedPathSegment("v2")
+                .addEncodedPathSegment("tournaments")
+                .addEncodedPathSegment(tournamentID)
+                .addEncodedPathSegment(matchID)
+                .addEncodedPathSegment("streams");
+        }
+        RequestBody body = RequestBody.create(MediaType.parse("application/json"),streamList.toString());
+        Request request = client.getAuthenticatedRequestBuilder()
+            .put(body)
+            .url(url.build())
+            .build();
+
+        try {
+            String responseBody = client.executeRequest(request).body().string();
+            return mapper.readValue(responseBody,
+                mapper.getTypeFactory().constructCollectionType(List.class,String.class));
+        } catch (IOException | NullPointerException e) {
+            System.out.println(e.getMessage());
+            throw new ToornamentException("Got IOException updating match streams");
         }
     }
 }
--- src/main/java/com/toornament/model/Video.java
+++ src/main/java/com/toornament/model/Video.java
@@ -3,7 +3,6 @@ package com.toornament.model;
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
 @Getter
