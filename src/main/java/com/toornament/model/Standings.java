d0a2d43ddfdb9c361f1ed5acd048eae946931cd9
Brenton Poke <brentonpoke@outlook.com>
2020-02-27 21:08:02 -0500

    toString()

--- src/main/java/com/toornament/model/Standings.java
+++ src/main/java/com/toornament/model/Standings.java
@@ -2,11 +2,17 @@
 package com.toornament.model;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
-@NoArgsConstructor
-@Data
+@Getter
+@Setter
+@EqualsAndHashCode
 @SuppressWarnings("unused")
 public class Standings {
 
@@ -16,5 +22,12 @@ public class Standings {
     private Integer rank;
     @JsonProperty("tournament_id")
     private String tournamentId;
-
+    public String toString() {
+        try {
+            return new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(this);
+        } catch (JsonProcessingException e) {
+            System.out.println(e.getMessage());
+        }
+        return null;
+    }
 }
