328ad25b248d95cd4ec4040597fe6b13f482cfbe
Brenton <brentonpoke@outlook.com>
2021-09-26 05:35:53 -0400

    deleting former scopes

--- src/main/java/com/toornament/model/enums/Scope.java
+++ src/main/java/com/toornament/model/enums/Scope.java
@@ -4,15 +4,11 @@ import com.fasterxml.jackson.annotation.JsonValue;
 
 public enum Scope {
 
-    MANAGE("participant:manage_registrations"),
-    USER_INFO("user:info"),
     ORGANIZER_VIEW("organizer:view"),
     ORGANIZER_ADMIN("organizer:admin"),
     ORGANIZER_RESULT("organizer:result"),
     ORGANIZER_PARTICIPANT("organizer:participant"),
     ORGANIZER_DELETE("organizer:delete"),
-    MANAGE_PARTICIPANTS("participant:manage_participations"),
-    MANAGE_REGISTRATIONS("participant:manage_registrations"),
     ORGANIZER_REGISTRATION("organizer:registration"),
     ORGANIZER_PERMISSION("organizer:permission");
     private final String scope;
